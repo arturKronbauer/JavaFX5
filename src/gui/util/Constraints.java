3229884cacf0b682ae8e317431ac3fd7e45f3252
Artur Henrique Kronbauer <arturhk@gmail.com>
2023-05-31 00:23:43 -0300

    Limitações para TextField e Initializable

---
+++ src/gui/util/Constraints.java
@@ -0,0 +1,33 @@
+package gui.util;
+
+import javafx.scene.control.TextField;
+
+public class Constraints {
+
+	// cria um listener para identificar se em um TextField só está sendo digitado números inteiros 
+	public static void setTextFieldInteger(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	// cria um listener para limitar o valor máximo de caracteres de um TextField 
+	public static void setTextFieldMaxLength(TextField txt, int max) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && newValue.length() > max) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	// cria um listener para identificar se em um TextField só está sendo digitado números reais 
+	public static void setTextFieldDouble(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+}
